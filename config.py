137e76caa76bceb81c85e2dda793301d87d02359
crazyjaatabhi <jaattabhisheksinghsolanki@gmail.com>
2025-11-24 06:49:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,17 +10,17 @@
 import os
 
 # Required configurations (loaded from environment variables)
-API_ID = int(os.getenv("API_ID", 0))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_URI = os.getenv("MONGO_URI", "")
+API_ID = int(os.getenv("4202458936", 0))
+API_HASH = os.getenv("1a413a1530d4202458936bb0c7c92d8a", "")
+BOT_TOKEN = os.getenv("8238269712:AAGR4MMqrBXnHfb3gWC1IBsz336f_UPXmrs", "")
+MONGO_URI = os.getenv("mongodb+srv://crazyjaatabhi_86: crazyjaatabhi_86@crazyjaatabhi.wbikzkk.mongodb.net/?appName=crazyjaatabhi", "")
 DB_NAME = os.getenv("DB_NAME", "Cluster0")
 
 # Owner and bot details
-OWNER_ID = int(os.getenv("OWNER_ID", 0))
-BOT_USERNAME = os.getenv("BOT_USERNAME", "NomadeHelpBot")
+OWNER_ID = int(os.getenv("8418386439", 0))
+BOT_USERNAME = os.getenv("@Atanki_bot", "NomadeHelpBot")
 
 # Links and visuals
-SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/learning_bots")
-UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "https://t.me/learningbots79")
-START_IMAGE = os.getenv("START_IMAGE", "https://files.catbox.moe/j2yhce.jpg")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/atankigodfather")
+UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "https://t.me/atankigod")
+START_IMAGE = os.getenv("START_IMAGE", "https://graph.org/file/5cf6daecfbbccc395e745-4e238d8a5fa04921a0.jpg")
